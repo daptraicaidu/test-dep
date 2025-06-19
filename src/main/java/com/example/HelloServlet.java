e09951d1f8a137206d2bd5c1a8097000a61baaec
daptraicaidu <trancuop8@gmail.com>
2025-06-19 14:26:27 +0700

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -5,12 +5,12 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet("/hello")
+@WebServlet("/hello9")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World, I am a servlet, 12.06.2025!</h1>");
+        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của feature hello-19.06.2025!</h1>");
     }
 }
